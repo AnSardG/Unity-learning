0d6c79e26100010c36735b2789274112cf8068eb
Antonio Sard González <antoniosardgonzalez@gmail.com>
2023-06-10 17:19:38 +0200

    Add lot of crazy things

---
+++ Jueguito/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float jumpHeight = 4;
+    [SerializeField] float timeToJumpApex = .4f;
+    [SerializeField] float moveSpeed = 6;
+
+    float accelerationTimeAirborne = .1f;
+    float accelerationTimeGrounded = .04f;
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    Controller2D controller;
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+
+    private void Update()
+    {
+        if(controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if(Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
